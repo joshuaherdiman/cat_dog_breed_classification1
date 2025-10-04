03228e77cb54cd2587238928c4aace9355981e64
joshuaherdiman <joshuaherdiman@gmail.com>
2025-10-04 10:26:05 +0700

    The PILImage now has the functionality of being able to create new Images! (Please note that there is a bit of trouble running the program, so do expect errors)
    -Joshua =)

--- main.py
+++ main.py
@@ -7,16 +7,29 @@ st.text("Built By Joshua")
 
 breed_classification_model = load_learner("breed-classification(1).pkl")
 
-def predict(image):
-    img = PILImage.create(image)
-    outputs = breed_classification_model.predict(img)
-    breed_name = outputs[1].item()
-    return(breed_name)
+def extract_breed(file_name):
+    p = Path(file_name)
+    breed_name_parts = p.stems.split("_")
 
+    final_breed_name = " "
+    length_parts = len(breed_name_parts)-1
+    for i in range(length_parts):
+        final_breed_name += breed_name_parts[i]
+        if i != length_parts:
+            final_breed_name += "_"
+
+    return final_breed_name
+
+def predict(image):
+    img = PILImage.creat(image)
+    pred_class, pred_idx, outputs = breed_classification_model.predict(img)
+    return pred_class
 
 uploaded_file = st.file_uploader("Choose an image to upload...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
     prediction = predict(uploaded_file)
-    st.write(prediction)
+    st.subheader(f"Predicted Breed: {prediction}")
+
+st.text("Built with Streamlit and FastAI.")
